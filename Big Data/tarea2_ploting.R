876465ee9bd390e8fdfbea8869f00877b8dd148f
unknown <diabbluis@gmail.com>
2021-11-06 15:37:38 -0300

    Time series plot

--- Big Data/tarea2_ploting.R
+++ Big Data/tarea2_ploting.R
@@ -2,12 +2,13 @@ library(tidyverse)
 library(here)
 library(ggtext)
 
-theme_set(theme_light(base_family = "Amasis MT Std", base_size = 16))
+theme_set(theme_minimal(base_family = "Amasis MT Std", base_size = 16))
 theme_update(
   text = element_text(color = "#111111"),
   plot.title = element_markdown(hjust = .5),
   plot.subtitle = element_markdown(hjust = .5),
-  strip.text = element_text(color = "#111111", face = "italic", size = "16")
+  strip.text = element_text(color = "#111111", face = "italic", size = "16"),
+  panel.grid.minor.x = element_blank()
 )
 
 clean_data <- read_csv(file = here("Big Data", "Data", "clean_data.csv"))
@@ -26,4 +27,13 @@ sorted_pct(COLOR)
 sorted_pct(REPORTED_YEAR, wt = NUMBER_OF_TRIPS)
 sorted_pct(REPORTED_YEAR, REPORTED_MONTH, wt = NUMBER_OF_TRIPS)
 
-
+clean_data %>% 
+  group_by(DATE) %>% 
+  summarize(TRIPS = sum(NUMBER_OF_TRIPS)) %>% 
+  ggplot(aes(DATE, TRIPS)) +
+  geom_line(color = "#7030A0", size = 1) +
+  geom_smooth(method = "loess", formula = y ~ x, alpha = .5, color = "#b898d0", fill = "#b898d0") +
+  scale_y_continuous(labels = number_format()) +
+  scale_x_date(date_breaks = "6 months", date_labels = "%m-%Y") +
+  labs(title = "Número de viajes de TPNS en Chicago",
+       x = NULL, y = "Número de viajes")
