99e5ae6c2597c93e42627b9d1f94360e8acd71f9
unknown <diabbluis@gmail.com>
2021-11-06 13:24:57 -0300

    Remove chart config

--- Big Data/tarea2_cleaning.R
+++ Big Data/tarea2_cleaning.R
@@ -1,14 +1,5 @@
 library(tidyverse)
 library(here)
-library(ggtext)
-
-theme_set(theme_light(base_family = "Amasis MT Std", base_size = 16))
-theme_update(
-  text = element_text(color = "#111111"),
-  plot.title = element_markdown(hjust = .5),
-  plot.subtitle = element_markdown(hjust = .5),
-  strip.text = element_text(color = "#111111", face = "italic", size = "16")
-)
 
 ridesharing <- read_csv(file = here("Big Data", "Data", "chicago-ridesharing-vehicles.csv"))
 ridesharing %>% skimr::skim()
