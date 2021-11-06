2c718effa20d2bb444ccde78eafc8d52f06ec5e9
unknown <diabbluis@gmail.com>
2021-11-06 14:38:09 -0300

    Counting columns

--- Big Data/tarea2_ploting.R
+++ Big Data/tarea2_ploting.R
@@ -9,3 +9,21 @@ theme_update(
   plot.subtitle = element_markdown(hjust = .5),
   strip.text = element_text(color = "#111111", face = "italic", size = "16")
 )
+
+clean_data <- read_csv(file = here("Big Data", "Data", "clean_data.csv"))
+skimr::skim(clean_data)
+
+sorted_pct <- function(..., wt = NULL) {
+  clean_data %>% 
+    count(..., sort = TRUE) %>% 
+    mutate(pct = n / sum(n))
+}
+
+sorted_pct(STATE)
+sorted_pct(MAKE)
+sorted_pct(MAKE, MODEL)
+sorted_pct(COLOR)
+sorted_pct(REPORTED_YEAR, wt = NUMBER_OF_TRIPS)
+sorted_pct(REPORTED_YEAR, REPORTED_MONTH, wt = NUMBER_OF_TRIPS)
+
+
